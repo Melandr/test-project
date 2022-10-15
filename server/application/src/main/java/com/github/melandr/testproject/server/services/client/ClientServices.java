b48ae68df4e1a50d7cea41ccd36a4f8df5dcfd6c
ft030681gae <alexandr.gorbenko@monobank.ua>
2022-10-15 22:15:31 +0300

    /client/auth service - поправил формирование подписи

--- server/application/src/main/java/com/github/melandr/testproject/server/services/client/ClientServices.java
+++ server/application/src/main/java/com/github/melandr/testproject/server/services/client/ClientServices.java
@@ -1,6 +1,11 @@
 package com.github.melandr.testproject.server.services.client;
 
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
@@ -12,8 +17,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.github.melandr.testproject.server.services.client.Dto.*;
-
+import com.github.melandr.testproject.server.services.client.Dto.AuthRequest;
+import com.github.melandr.testproject.server.services.client.Dto.AuthResponse;
 import com.github.melandr.testproject.server.services.error.ErrorResponse;
 
 
@@ -22,19 +27,31 @@ class ClientServices {
 
 	private static final String ACCEPTABLE_USER_NAME = "user_1";
 	private static final String ACCEPTABLE_USER_PASSWORD = "melandr3";
-	
+
 	private static final String MD5_HASH_PASSWORD = DigestUtils.md5DigestAsHex(ACCEPTABLE_USER_PASSWORD.getBytes(StandardCharsets.UTF_8));
-	
+
+    private static final Map<String, String> SIGNS = Collections.synchronizedMap(new HashMap<>());
+
 	@PostMapping(value = "/client/auth", produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ResponseBody
-	ResponseEntity clientAuth(@RequestBody AuthRequest request) {	
-		if(StringUtils.isBlank(request.getName()) || StringUtils.isBlank(request.getPassword())) {
+	ResponseEntity clientAuth(@RequestBody AuthRequest request) {
+        String userName = request.getName();
+
+        if (StringUtils.isBlank(userName) || StringUtils.isBlank(request.getPassword())) {
 			return new ResponseEntity<ErrorResponse>(new ErrorResponse("Field(s) content is null or empty!"), HttpStatus.BAD_REQUEST);
 		}
-		if(ACCEPTABLE_USER_NAME.equals(request.getName()) && MD5_HASH_PASSWORD.equals(request.getPassword())) {
-			return new ResponseEntity<AuthResponse>(new AuthResponse("wery_well_sign"), HttpStatus.OK);
+
+        if (ACCEPTABLE_USER_NAME.equals(userName) && MD5_HASH_PASSWORD.equals(request.getPassword())) {
+            if (!SIGNS.containsKey(userName)) {
+                String phrase = userName + "#@#" + request.getPassword() + "_17QJcv@!~zc"
+                        + DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(LocalDateTime.now());
+                SIGNS.put(userName, DigestUtils.md5DigestAsHex(phrase.getBytes(StandardCharsets.UTF_8)));
+            }
+
+            return new ResponseEntity<AuthResponse>(
+                    new AuthResponse(SIGNS.get(request.getName())), HttpStatus.OK);
 		}
-		
+
 		return new ResponseEntity<AuthResponse>(new AuthResponse("user or password is fail!"), HttpStatus.NOT_FOUND);
 	}
 
