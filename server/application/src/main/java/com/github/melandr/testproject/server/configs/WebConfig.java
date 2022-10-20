4e2ef67062c0b24dc3f0760c07598891391c3462
ft030681gae <alexandr.gorbenko@monobank.ua>
2022-10-20 20:19:30 +0300

    try fix cors

--- server/application/src/main/java/com/github/melandr/testproject/server/configs/WebConfig.java
+++ server/application/src/main/java/com/github/melandr/testproject/server/configs/WebConfig.java
@@ -31,7 +31,7 @@ class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+        registry.addMapping("/**").exposedHeaders("Access-Control-Allow-Origin");
     }
 
     @Bean
