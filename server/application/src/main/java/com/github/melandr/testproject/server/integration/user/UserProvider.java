0942d74f989471ac83cff64b4553387884a5801d
ft030681gae <alexandr.gorbenko@monobank.ua>
2022-11-20 16:02:41 +0200

    db

--- server/application/src/main/java/com/github/melandr/testproject/server/integration/user/UserProvider.java
+++ server/application/src/main/java/com/github/melandr/testproject/server/integration/user/UserProvider.java
@@ -1,11 +1,11 @@
 package com.github.melandr.testproject.server.integration.user;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.PreparedStatement;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import com.github.melandr.testproject.server.protocol.user.UserI;
@@ -14,24 +14,26 @@ import com.github.melandr.testproject.server.protocol.user.UserProviderI;
 @Component
 public class UserProvider implements UserProviderI {
 
-    private final Map<String, UserI> users;
-
     @Autowired
-    UserProvider() {
-        Map<String, UserI> users = new HashMap<>(2);
-        users.put("user_1", new User("user_1", "melandr3".getBytes(StandardCharsets.UTF_8), "Иванов Иван Иванович"));
-        users.put("admin_7", new User("admin_7", "hardcore_pass".getBytes(StandardCharsets.UTF_8), "Абдурахман Ибн Хоттаб"));
-        this.users = Collections.unmodifiableMap(users);
-    }
+    private JdbcTemplate template;
 
-//    @Override
-//    public Collection<UserI> getUsers() {
-//        return users.values();
-//    }
+//        new User("user_1", "melandr3".getBytes(StandardCharsets.UTF_8), "Иванов Иван Иванович");
+//        new User("admin_7", "hardcore_pass".getBytes(StandardCharsets.UTF_8), "Абдурахман Ибн Хоттаб");
 
     @Override
     public UserI getUserByLogin(String login) {
-        return users.get(login);
+        return template.query(con -> {
+            PreparedStatement ps = con.prepareStatement("select LOGIN, PASSWORD, FIRST_NAME, LAST_NAME, MIDDLE_NAME from USERS where LOGIN LIKE ?");
+            ps.setString(1, login);
+            return ps;
+        }, rs -> {
+            User user = null;
+            if(rs.next()) {
+                user = new User(rs.getString("LOGIN"), rs.getString("PASSWORD").getBytes(StandardCharsets.UTF_8),
+                        StringUtils.join(new String[]{rs.getString("FIRST_NAME"), rs.getString("LAST_NAME"), rs.getString("MIDDLE_NAME")}, " "));
+            }
+            return user;
+        });
     }
 
 }
