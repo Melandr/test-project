38f578a6c28d24e2aad677317794b01c4366b8c7
ft030681gae <alexandr.gorbenko@monobank.ua>
2022-11-20 16:11:10 +0200

    db

--- server/application/runDev.sh
+++ server/application/runDev.sh
@@ -6,4 +6,7 @@ mvn clean install
 cd ./application
 
 java -agentlib:jdwp=transport=dt_socket,server=y,address=localhost:8028,suspend=n \
+    -Dspring.datasource.url=jdbc:h2:tcp://localhost/~/Programs/h2/data/serverDb \
+    -Dspring.datasource.username=admin \
+    -Dspring.datasource.password=securityPassword \
     -jar ./target/serverSide.jar > server.log
