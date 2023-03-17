3b9251618bab861b2964a3c7c30fde774791ce2b
Melandr <melnychenko.a@gmail.com>
2023-03-17 08:46:57 +0200

    delete static table

--- client/src/app/pages/table-page.component.js
+++ client/src/app/pages/table-page.component.js
@@ -20,17 +20,21 @@ class TablePageComponent extends WFMComponent {
     onInit() {
         super.onInit();
 
+        console.log(this);
         // const table = $(this.selector);
-        // const table = this.el.find("#table-users");
+        // const table = this.find(".table-users__block");
+        // console.log(table);
+
         this.getUsers().then((data) => console.log(data.users.length));
     }
 
     afterInit() {
-        const table = this.el.find("#table-users").nativeElement;
-        console.log(table.rows);
-        let newRow = table.deleteRow(1);
-        console.log(table.rows);
-        this.render();
+        // const table = this.el.find("#table-users").nativeElement;
+        const table = this.el.find(".table-users__block");
+        console.log(table);
+        // let newRow = table.deleteRow(1);
+        // console.log(table.rows);
+        // this.render();
     }
 
     getUsers() {
@@ -58,60 +62,9 @@ class TablePageComponent extends WFMComponent {
 export const tablePageComponent = new TablePageComponent({
     selector: "table-of-users",
     template: `
-        <table class="users" id="table-users">
-            <tbody>
-                <tr>
-                    <th>Id</th>
-                    <th>Логин</th>
-                    <th>Фамилия</th>
-                    <th>Имя</th>
-                    <th>Отчество</th>
-                    <th>Телефон</th>
-                    <th>Email</th>
-                    <th>Фото</th>
-                </tr>
-                <tr>
-                    <td>Microsoft</td>
-                    <td>20.3</td>
-                    <td>30.5</td>
-                    <td>23.5</td>
-                    <td>40.3</td>
-                    <td>40.3</td>
-                    <td>40.3</td>
-                    <td>40.3</td>
-                </tr>
-                <tr>
-                    <td>Google</td>
-                    <td>50.2</td>
-                    <td>40.63</td>
-                    <td>45.23</td>
-                    <td>39.3</td>
-                    <td>39.3</td>
-                    <td>39.3</td>
-                    <td>39.3</td>
-                </tr>
-                <tr>
-                    <td>Apple</td>
-                    <td>25.4</td>
-                    <td>30.2</td>
-                    <td>33.3</td>
-                    <td>36.7</td>
-                    <td>36.7</td>
-                    <td>36.7</td>
-                    <td>36.7</td>
-                </tr>
-                <tr>
-                    <td>IBM</td>
-                    <td>20.4</td>
-                    <td>15.6</td>
-                    <td>22.3</td>
-                    <td>29.3</td>
-                    <td>29.3</td>
-                    <td>29.3</td>
-                    <td>29.3</td>
-                </tr>
-            </tbody>
-        </table>
+    <div class="table-users__block">
+
+    </div>            
         <div class="link__block">
             <a href="#not-existing-path" class="js-link nav__link">Перейти на главную</a>  
         </div>
